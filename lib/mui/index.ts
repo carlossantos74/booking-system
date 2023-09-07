2cb053a804797654caac83f73ee69829ea8459f5
Carlos Santos <carlos.santos74@outlook.com>
2023-09-07 17:51:50 -0300

    chore(FE - config): add theme provider

---
+++ lib/mui/index.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+  typography: {
+    h1: { fontSize: "2.5rem" },
+    h2: { fontSize: "2.2rem" },
+    h3: { fontSize: "1.75rem" },
+    h4: { fontSize: "1.5rem" },
+    h5: { fontSize: "1.25rem" },
+    h6: { fontSize: "1rem" },
+    body1: { fontSize: "1rem" },
+    body2: { fontSize: "0.875rem" },
+    caption: { fontSize: "0.75rem" },
+    button: { fontSize: "0.875rem" },
+  }
+})
--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 /* Core */
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+
 
 /* Instruments */
-import { reduxStore } from '@/lib/redux'
+import { reduxStore } from '@/lib/redux';
+import { theme } from '@/lib/mui'
 
 export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={reduxStore}>{props.children}</Provider>
+  return (
+    <Provider store={reduxStore}>
+      <ThemeProvider theme={theme}>
+        {props.children}
+      </ThemeProvider>
+    </Provider>
+  )
 }
