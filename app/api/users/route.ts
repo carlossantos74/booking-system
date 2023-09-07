0177bdc7c4078ed37394855702a0d05474343953
Carlos Santos <carlos.santos74@outlook.com>
2023-09-07 16:43:09 -0300

    feat(BE): add users module

---
+++ app/api/users/route.ts
@@ -0,0 +1,144 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/lib/prisma';
+import { User } from '@prisma/client';
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const url = new URL(request.url);
+  const id = url.searchParams.get('id');
+
+  try {
+    if(id) { 
+      const user = await db.user.findUnique({
+        where: {
+          id,
+        }
+      });
+  
+      if(!user) {
+        return NextResponse.json({
+          statusText: 'Not Found'
+        }, 
+        { status: 404 });
+      }
+  
+      return NextResponse.json<User>(user);
+    }
+  
+    const users = await db.user.findMany();
+  
+    return NextResponse.json<User[]>(users)  
+  } catch (error) {
+    return NextResponse.json({
+      statusText: 'Internal Server Error'
+    }, { status: 500 })
+  }
+}
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const body = await request.text();
+
+  if(!body) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing body'
+    }, 
+    { status: 400 });
+  };
+
+  const { name } = JSON.parse(body);
+
+  if(!name) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing name'
+    }, 
+    { status: 400 });
+  }
+
+  try {
+    const user = await db.user.create({
+      data: {
+        name
+      }
+    });
+
+    return NextResponse.json<User>(user, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({
+      statusText: 'Internal Server Error'
+    }, { status: 500 });
+  }
+}
+
+export async function PUT(request: NextRequest): Promise<NextResponse> {
+  const body = await request.text();
+
+  if(!body) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing body'
+    }, 
+    { 
+      status: 400 
+    });
+  };
+
+  const { name, id } = JSON.parse(body);
+
+  if(!name || !id) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing fields'
+    }, 
+    { 
+      status: 400 
+    });
+  }
+
+  try {
+    const user = await db.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name
+      }
+    });
+
+    return NextResponse.json<User>(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      statusText: 'Internal Server Error'
+    }, { status: 500 });
+  }
+}
+
+export async function DELETE(request: NextRequest): Promise<NextResponse> {
+  const body = await request.text();
+
+  if(!body) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing body'
+    }, 
+    { status: 400 });
+  };
+
+  const { id } = JSON.parse(body);
+
+  if(!id) {
+    return NextResponse.json({
+      statusText: 'Bad Request - Missing id'
+    }, 
+    { status: 400 });
+  }
+
+  try {
+    await db.user.delete({
+      where: {
+        id,
+      },
+    })
+
+    return NextResponse.json({}, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      statusText: 'Internal Server Error'
+    }, { status: 500 });
+  }
+}
---
+++ prisma/migrations/20230907144039_init/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
